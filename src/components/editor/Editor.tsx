cc5fcd970b092b9002075e8e5e0bbcf196d527b2
Hamza El Meknassi <elmeknassi.h@gmail.com>
2022-03-12 21:21:13 +0100

    Added editor tools

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,13 +1,49 @@
 import React from "react";
 import EditorJS from "@editorjs/editorjs";
+const Header = require('@editorjs/header');
+const NestedList = require('@editorjs/nested-list');
+const Marker = require('@editorjs/marker');
+const CodeTool = require('@editorjs/code');
+const Table = require('@editorjs/table');
+const Checklist = require('@editorjs/checklist');
 
 export default class Editor extends React.Component {
     editor: EditorJS;
 
     componentDidMount() {
         this.editor = new EditorJS({
+            onReady: () => {
+            },
             holder: "editor",
-            placeholder: 'Start typing your note...'
+            placeholder: 'Start typing your note...',
+            tools: {
+                header: {
+                    class: Header,
+                    shortcut: 'CMD+SHIFT+H',
+                    config: {
+                        placeholder: 'Header',
+                        levels: [1, 2, 3, 4, 5, 6],
+                        defaultLevel: 1
+                    }
+                },
+                list: {
+                    class: NestedList,
+                    inlineToolbar: true,
+                    config: {
+                        defaultStyle: 'unordered'
+                    }
+                },
+                marker: {
+                    class: Marker,
+                    shortcut: 'CMD+SHIFT+M',
+                },
+                code: CodeTool,
+                table: Table,
+                checklist: {
+                    class: Checklist,
+                    inlineToolbar: true,
+                },
+            },
         })
     }
 
--- src/components/notesList/NotesList.tsx
+++ src/components/notesList/NotesList.tsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export default class NotesList extends React.Component {
     render() {
-        return <h1>Hello, this is NotesList</h1>;
+        return <>
+            <h1>Hello, this is NotesList</h1>
+            <h2>Hello, this is NotesList</h2>
+        </>;
     }
 }
